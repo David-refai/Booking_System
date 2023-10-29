1dfacbd112f59dc54ba6cfbecc239bc3ead6d370
David-refai <David.refai@hotmail.com>
2023-10-29 22:31:02 +0100

    add Slack 2

--- frontend/booking-system-react/src/components/user/Client.js
+++ frontend/booking-system-react/src/components/user/Client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const GetUser = async () => {
   try {
     // after the proxy is set up in vite.config.js, this will work
-      return await axios.get('api/v1/users/');
+      return await axios.get('api/v1/users');
   
   } catch (error) {
     console.log(error);
